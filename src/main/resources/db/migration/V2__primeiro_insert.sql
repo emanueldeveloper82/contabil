6c885c381b3224d216fcef4a68429c5dd228b66e
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-27 13:12:28 -0300

    Primeiras migrações;

---
+++ src/main/resources/db/migration/V2__primeiro_insert.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO contabilidade.conta
+(data_cadastro, data_pagamento, data_vencimento, nome, qtd_dias_atraso, valor_corrigido, valor_original)
+VALUES('2022-01-27', '2022-01-26', '2022-01-25', 'Conta01', 1, 102.10, 100.00);
+
