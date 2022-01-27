61d90545b9831625c67bfab29f13b8a8e3f38665
emanuel.developer82 <emanuel.developer82@gmail.com>
2022-01-27 10:32:38 -0300

    Ajustes no método salvar e inclusão dos métodos de calculo de juros e multa;

--- src/main/java/br/com/contabil/service/impl/ContaServiceImpl.java
+++ src/main/java/br/com/contabil/service/impl/ContaServiceImpl.java
@@ -9,6 +9,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +31,6 @@ public class ContaServiceImpl implements ContaService {
         return repository.findAll();
     }
 
-
     /**
      * Método que salva ou atualiza uma conta a pagar;
      * @param contaDTO
@@ -37,11 +39,15 @@ public class ContaServiceImpl implements ContaService {
     @Override
     public ResponseEntity<?> salvar(ContaDTO contaDTO) {
 
+        Conta conta = modelMapper.map(contaDTO, Conta.class);
 
         try {
-            Conta conta = repository.save(modelMapper.map(contaDTO, Conta.class));
+            conta.setDataCadastro(LocalDate.now());
+            conta.setQtdDiasAtraso(ChronoUnit.DAYS.between(conta.getDataVencimento(), conta.getDataPagamento()));
+            conta.setValorCorrigido(calcularValorCorrigido(conta));
+
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(modelMapper.map(conta, ContaDTO.class));
+                    .body(modelMapper.map(repository.save(conta), ContaDTO.class));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Erro ao tentar salvar uma conta.");
         }
@@ -60,8 +66,37 @@ public class ContaServiceImpl implements ContaService {
             repository.delete(modelMapper.map(contaDTO, Conta.class));
             return ResponseEntity.ok().body("Conta removida com sucesso.");
         } else {
-            return ResponseEntity.badRequest().body("Erro ao tentar excluir uma conta.");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body("Conta não encontrada.");
         }
     }
+
+
+    private BigDecimal calcularValorCorrigido(Conta conta) {
+        return conta.getValorOriginal()
+                .add(conta.getValorOriginal().multiply(regraMulta(conta.getQtdDiasAtraso()))
+                        .add(conta.getValorOriginal().multiply(regraJuros(conta.getQtdDiasAtraso()))));
+    }
+
+    private BigDecimal regraMulta(Long atraso) {
+        if (atraso <= 3) {
+            return new BigDecimal("0.02");
+        }else if (atraso > 3 && atraso <= 5) {
+            return new BigDecimal("0.03");
+        }else {
+            return new BigDecimal("0.05");
+        }
+    }
+
+    private BigDecimal regraJuros(Long atraso) {
+        if (atraso <= 3) {
+            return new BigDecimal("0.001");
+        }else if (atraso > 3 && atraso <= 5) {
+            return new BigDecimal("0.002");
+        }else {
+            return new BigDecimal("0.003");
+        }
+    }
+
 
 }
